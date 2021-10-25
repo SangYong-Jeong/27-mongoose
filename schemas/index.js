d1d196f3c8e5ecc535dcbd416591300ac34e4372
SangYong-Jeong <mmaf@naver.com>
2021-10-25 15:16:03 +0900

    Making debuggin setting + schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const path = require('path')
+const fs = require('fs-extra')
 const { DB_HOST, DB_USER, DB_PASS } = process.env
 const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}?retryWrites=true&w=majority`
 
@@ -8,9 +10,15 @@ module.exports = async () => {
     const connect = await mongoose.connect(url)
     console.log('mongoose server start')
 
-    const Users = require('./Users')
-    const Books = require('./Books')
-    return { Users, Books }
+    const db = {}
+    fs
+      .readdirSync(__dirname)
+      .filter(file => file !== 'index.js')
+      .forEach(file => {
+        const model = require(path.join(__dirname, file));
+        db[model.modelName] = model
+      })
+    return db
   }
   catch (err) {
     console.log(err)
