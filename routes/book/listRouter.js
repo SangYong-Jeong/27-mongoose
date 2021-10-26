5e91e53ff3b16615670e34ee6369b0af80d76629
SangYong-Jeong <mmaf@naver.com>
2021-10-26 10:59:03 +0900

    CRUD 에서 CUD 배움

--- routes/book/listRouter.js
+++ routes/book/listRouter.js
@@ -19,4 +19,51 @@ router.use('/create', async (req, res, next) => {
   }
 })
 
+router.get('/delete/:id', async (req, res, next) => {
+  try {
+    const book = await Books.findByIdAndDelete(req.params.id)
+    res.status(200).json(book)
+  }
+  catch (err) {
+    next(createError(err))
+  }
+})
+
+router.use('/list', async (req, res, next) => {
+  try {
+    const books = await Books.find()
+    res.json(books)
+  }
+  catch (err) {
+    next(createError(err))
+  }
+})
+
+router.use('/update', async (req, res, next) => {
+  try {
+    const newBook = {
+      id: '6176517b9d013d01f0a6b9ff',
+      writer: '허생',
+      title: '허생전',
+      content: '허생이 하루는 쥐가 되어...'
+    }
+    // const { id, title, writer, content } = req.body
+    const { id, title, writer, content } = newBook
+
+    const book = await Books.findByIdAndUpdate(
+      id, 
+      {
+        title, 
+        writer, 
+        content 
+      }, {
+        new: true,
+      })
+    res.status(200).json(book)
+  }
+  catch (err) {
+    next(createError(err))
+  }
+})
+
 module.exports = router
